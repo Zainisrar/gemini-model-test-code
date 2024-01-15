da6574c324047bb6af03a13419c337331c6104a5
Zainisrar <127589655+Zainisrar@users.noreply.github.com>
2024-01-15 09:51:42 -0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,22 @@
+import pathlib
+import textwrap
+
+import google.generativeai as genai
+
+from IPython.display import display
+from IPython.display import Markdown
+
+
+def to_markdown(text):
+  text = text.replace('•', '  *')
+  return Markdown(textwrap.indent(text, '> ', predicate=lambda _: True))
+# Or use `os.getenv('GOOGLE_API_KEY')` to fetch an environment variable.
+GOOGLE_API_KEY='AIzaSyCljBZvB7av5p9VqZQFFBJuwuCnrA_dP_w'
+
+genai.configure(api_key=GOOGLE_API_KEY)
+for m in genai.list_models():
+  if 'generateContent' in m.supported_generation_methods:
+    print(m.name)
+model = genai.GenerativeModel('gemini-pro')
+response = model.generate_content("write a 1000 words blog on pakistan")
+print(response.text)
